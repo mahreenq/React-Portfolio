d6bb62968051cf5c9cfd19edbeb357225e2cf2b2
Mahreen Qureshi <mahreenq@hotmail.com>
2017-12-01 00:48:47 -0500

    modified footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -9,8 +9,9 @@ class Footer extends Component {
       return (
         <div className="footer">
               <div className="contactFooter">
-               <a href="https://github.com/mahreenq"> <p> <i className="fa fa-github" aria-hidden="true"></i> </p> </a>
-               <a href="mailto:mahreenq@hotmail.com"><p><i className="fa fa-envelope" aria-hidden="true"></i></p></a>
+               <a target="_blank" href="https://github.com/mahreenq"> <p> <i className="fa fa-github" aria-hidden="true"></i> </p> </a>
+               <a target="_blank" href="https://www.linkedin.com/in/mahreen-qureshi/"> <p> <i className="fa fa-linkedin" aria-hidden="true"></i> </p> </a>
+               <a target="_blank" href="mailto:mahreenq@hotmail.com"><p><i className="fa fa-envelope" aria-hidden="true"></i></p></a>
               </div>
               <p className="">© 2017 mahreenq.com</p>
 
